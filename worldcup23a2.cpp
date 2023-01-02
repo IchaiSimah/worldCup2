111e7e40be0fa27e710f1451c38d6d13913140b1
RubenF <rubenf@campus.technion.ac.il>
2023-01-02 16:16:27 +0200

    il manque que getpartialspirit et un peu de addplayercards

--- worldcup23a2.cpp
+++ worldcup23a2.cpp
@@ -51,7 +51,7 @@ StatusType world_cup_t::remove_team(int teamId){
 StatusType world_cup_t::add_player(int playerId, int teamId, const permutation_t &spirit, int gamesPlayed,
                                    int ability, int cards, bool goalKeeper){
 
-    if (playerId <= 0 || teamId <= 0 /*|| if spirit not good */ ||gamesPlayed < 0|| cards < 0 ) return StatusType::INVALID_INPUT;
+    if (playerId <= 0 || teamId <= 0 || !spirit.isvalid() ||gamesPlayed < 0|| cards < 0 ) return StatusType::INVALID_INPUT;
     if (playersTable.find(playerId)!=-1 || AVL_team_by_id.find(teamId)== nullptr) return  StatusType::FAILURE;
 
     shared_ptr<Team> team = AVL_team_by_id.find(teamId)->data;
